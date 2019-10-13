6815d03424290a8d947e86f2adb96e7ab27c0a8d
P Skolsky <patrickskolskyart@gmail.com>
2019-10-13 12:09:09 -0400

    changed nav, odd behaviors with each dropdown item.  fav dropdown and rec dropdown display items different.  also, it appears that items in favorites are showing 3 times repeated.  also, when you remove an item from favotrites, the ENTIRE fav dropdrown menu gets removed and cannot be accessed again.

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,13 +13,13 @@ var movieArray = [];
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyC3YGy5JfZHdYFNGh8PRicPvSJnXPEtryc",
-authDomain: "medo-3c7b5.firebaseapp.com",
-databaseURL: "https://medo-3c7b5.firebaseio.com",
-projectId: "medo-3c7b5",
-storageBucket: "medo-3c7b5.appspot.com",
-messagingSenderId: "759891052026",
-appId: "1:759891052026:web:28e21af9c79d0bc71bd043"
+    apiKey: "AIzaSyC3YGy5JfZHdYFNGh8PRicPvSJnXPEtryc",
+    authDomain: "medo-3c7b5.firebaseapp.com",
+    databaseURL: "https://medo-3c7b5.firebaseio.com",
+    projectId: "medo-3c7b5",
+    storageBucket: "medo-3c7b5.appspot.com",
+    messagingSenderId: "759891052026",
+    appId: "1:759891052026:web:28e21af9c79d0bc71bd043"
 };
 
 // Initialize Firebase
@@ -28,33 +28,33 @@ var database = firebase.database();
 
 
 // Functions
-function addMovie() {                       // Adds a movie item to the music medo
+function addMovie() { // Adds a movie item to the music medo
     var userInput = $('#search').val();
-    
+
     if ($('#search').val() === '') {
         return;
-    } else if (movieArray.includes(userInput)) {        
+    } else if (movieArray.includes(userInput)) {
         return;
-    } else {  
-        movieArray.push(userInput);  
+    } else {
+        movieArray.push(userInput);
         database.ref('Watch/').push({
             movie: userInput
-        });       
-    }   
-}  
+        });
+    }
+}
 
-function addMusic() {                       // Adds a music item to the music medo
-    var userInput = $('#search').val(); 
+function addMusic() { // Adds a music item to the music medo
+    var userInput = $('#search').val();
 
     if ($('#search').val() === '') {
         return;
-    } else if (musicArray.includes(userInput)) {       
+    } else if (musicArray.includes(userInput)) {
         return;
-    } else {  
-        musicArray.push(userInput);  
+    } else {
+        musicArray.push(userInput);
         database.ref('Listen/').push({
             artist: userInput
-        });       
+        });
     }
 }
 
@@ -62,7 +62,7 @@ function movieMEDO() {
     var moviePicked = $(this).attr('data-name');
     var APIkey = '95a6c9d4de568b3ebaa4ea26320798b4';
     var queryURL = 'https://api.themoviedb.org/3/search/movie?api_key=' + APIkey + '&query=' + moviePicked;
-        
+
     $.ajax({
         url: queryURL,
         method: 'GET'
@@ -78,16 +78,16 @@ function movieMEDO() {
         $('#movie-title').text(movieTitle);
         $('#movie-image').attr({
             'src': posterExt.toString(),
-            'width': '25%', 
+            'width': '25%',
             'height': '25%'
-            });
+        });
         $('#movie-summary').html(movieSummary);
         $('#release-date').text(movieRelease);
 
         var queryDetails = movieObject.id;
-        var recommendationsURL = 'https://api.themoviedb.org/3/movie/' + queryDetails + '/recommendations?api_key=' + APIkey +'&language=en-US&page=1';
+        var recommendationsURL = 'https://api.themoviedb.org/3/movie/' + queryDetails + '/recommendations?api_key=' + APIkey + '&language=en-US&page=1';
         var youtubeURL = 'https://api.themoviedb.org/3/movie/' + queryDetails + '?api_key=' + APIkey + '&append_to_response=videos';
-    
+
         $.ajax({
             url: youtubeURL,
             method: 'GET'
@@ -131,17 +131,17 @@ function musicMEDO() {
     }).then(function(response) {
         var bandName = response.artist.name;
         var bandBio = response.artist.bio.summary;
-        var genre = response.artist.tags.tag;                       
-        var similarBands = response.artist.similar.artist;    
+        var genre = response.artist.tags.tag;
+        var similarBands = response.artist.similar.artist;
 
-        $('#band-bio').html(bandBio);                               
+        $('#band-bio').html(bandBio);
         $('#genre').empty();
-        for (var j = 0; j < genre.length; j++) {                    
+        for (var j = 0; j < genre.length; j++) {
             var genreLoop = $('<span>').text(genre[j].name + ' ');
             $('#genre').append(genreLoop);
         }
         $('#similar-bands').empty();
-        for (var i = 0; i < similarBands.length; i++) {             
+        for (var i = 0; i < similarBands.length; i++) {
             var newHREF = response.artist.similar.artist[i].url;
             var newLink = $('<a>').attr({
                 'href': newHREF,
@@ -151,7 +151,7 @@ function musicMEDO() {
             newLink.append(bandLoop);
             $('#similar-bands').append(newLink);
         }
-        
+
         $.ajax({
             url: lastfmDetails,
             method: 'GET'
@@ -167,106 +167,106 @@ function musicMEDO() {
     });
 }
 
-database.ref('Listen/').on('child_added', function(data) {      // LISTEN retrieves data from Firebase on page load
+database.ref('Listen/').on('child_added', function(data) { // LISTEN retrieves data from Firebase on page load
     var newArtist = data.val().artist;
     var key = data.key;
     var newListItem = $('<li>').attr('class', 'music-item list-group-item hvr-shutter-out-vertical');
-    var newListen = $('<span>').attr({                  
+    var newListen = $('<span>').attr({
         'class': 'music-name col-10',
-        'data-name': newArtist, 
+        'data-name': newArtist,
         'data-ref': key,
-        'data-toggle': "modal", 
+        'data-toggle': "modal",
         'data-target': "#musicModal"
-    });     
+    });
     var newRemove = $('<button>').text('X').attr({
-        'class': 'remove listen col-1',
+        'class': 'remove listen justify-content-end',
         'data-name': newArtist,
         'data-ref': key
-    }); 
+    });
     var newLike = $('<button>').text('Like').attr({
-        'class': 'like listen col-1',
+        'class': 'like listen justify-content-end',
         'data-name': newArtist,
         'data-ref': key
-    }); 
-    newListen.text(newArtist);                                              
+    });
+    newListen.text(newArtist);
     newListItem.append(newListen, newLike, newRemove);
     $('#music-medo').prepend(newListItem);
     $('#search').val('');
 });
 
 
-database.ref('Watch/').on('child_added', function(data) {      // WATCH retrieves data from Firebase on page load
+database.ref('Watch/').on('child_added', function(data) { // WATCH retrieves data from Firebase on page load
     var newMovie = data.val().movie;
     var key = data.key;
     var newListItem = $('<li>').attr('class', 'movie-item list-group-item hvr-shutter-out-vertical');
-    var newWatch = $('<span>').attr({                  
+    var newWatch = $('<span>').attr({
         'class': 'movie-name col-10',
-        'data-name': newMovie, 
+        'data-name': newMovie,
         'data-ref': key,
-        'data-toggle': "modal", 
+        'data-toggle': "modal",
         'data-target': "#movieModal"
-    });     
+    });
     var newRemove = $('<button>').text('X').attr({
         'class': 'remove watch col-1',
         'data-name': newMovie,
         'data-ref': key
-    }); 
+    });
     var newLike = $('<button>').text('Like').attr({
         'class': 'like watch col-1',
         'data-name': newMovie,
         'data-ref': key
-    }); 
-    newWatch.text(newMovie);                                              
+    });
+    newWatch.text(newMovie);
     newListItem.append(newWatch, newLike, newRemove);
     $('#movie-medo').prepend(newListItem);
     $('#search').val('');
 });
 
 
-database.ref('Favorites/').on('child_added', function(data) {      // FAVORITES retrieves data from Firebase on page load
+database.ref('Favorites/').on('child_added', function(data) { // FAVORITES retrieves data from Firebase on page load
     var newFavoriteArtist = data.val().favorite_artist;
     var newFavoriteMovie = data.val().favorite_movie;
     var key = data.key;
     var category = data.val();
 
     if (category.favorite_artist) {
-        var newListItem = $('<li>').attr('class', 'music-item list-group-item hvr-shutter-out-vertical');
-        var newListen = $('<span>').attr({                  
+        var newListItem = $('<button>').attr('class', 'music-item list-group-item hvr-shutter-out-vertical');
+        var newListen = $('<span>').attr({
             'class': 'music-name col-10',
-            'data-name': newFavoriteArtist, 
+            'data-name': newFavoriteArtist,
             'data-ref': key,
-            'data-toggle': "modal", 
+            'data-toggle': "modal",
             'data-target': "#musicModal"
-        });     
-        var musicTag = $('<span>').text('listened').attr('class', 'listen-tag'); 
+        });
+        var musicTag = $('<span>').text('listened').attr('class', 'listen-tag');
         var newRemove = $('<button>').text('X').attr({
             'class': 'remove listen col-1',
             'data-name': newFavoriteArtist,
             'data-ref': key
-        }); 
-        newListen.text(newFavoriteArtist);                                              
+        });
+        newListen.text(newFavoriteArtist);
         newListItem.append(newListen, musicTag, newRemove);
 
     } else if (category.favorite_movie) {
         var newListItem = $('<li>').attr('class', 'movie-item list-group-item hvr-shutter-out-vertical');
-        var newWatch = $('<span>').attr({                  
+        var newWatch = $('<span>').attr({
             'class': 'movie-name col-10',
-            'data-name': newFavoriteMovie, 
+            'data-name': newFavoriteMovie,
             'data-ref': key,
-            'data-toggle': "modal", 
+            'data-toggle': "modal",
             'data-target': "#movieModal"
-        });  
-        var movieTag = $('<span>').text('watched').attr('class', 'watch-tag');   
+        });
+        var movieTag = $('<span>').text('watched').attr('class', 'watch-tag');
         var newRemove = $('<button>').text('X').attr({
             'class': 'remove watch col-1',
             'data-name': newFavoriteMovie,
             'data-ref': key
-        }); 
-        newWatch.text(newFavoriteMovie);                                              
+        });
+        newWatch.text(newFavoriteMovie);
         newListItem.append(newWatch, movieTag, newRemove);
     }
 
-    $('#favorites-list').prepend(newListItem);
+    $('.dropdown-item').prepend(newListItem).addClass();
 });
 
 function removeItem() {
@@ -274,18 +274,18 @@ function removeItem() {
     var thisItem = $(this).closest('li');
     thisItem.detach();
     if (thisItem.hasClass('music-item')) {
-        database.ref('Listen/' + key).remove();                 //WORKING
-        database.ref('Favorites/' + key).remove();  
+        database.ref('Listen/' + key).remove(); //WORKING
+        database.ref('Favorites/' + key).remove();
         musicArray.splice(musicArray.indexOf(name), 1);
 
     } else if (thisItem.hasClass('movie-item')) {
-        database.ref('Watch/' + key).remove();                  //WORKING
-        database.ref('Favorites/' + key).remove();  
+        database.ref('Watch/' + key).remove(); //WORKING
+        database.ref('Favorites/' + key).remove();
         movieArray.splice(movieArray.indexOf(name), 1);
     }
 };
 
-function favoriteMedia() {                  // when the like button fires, this function movies the media to the favorites section
+function favoriteMedia() { // when the like button fires, this function movies the media to the favorites section
     var key = $(this).attr('data-ref');
     var name = $(this).attr('data-name');
     var thisItem = $(this).closest('li');
